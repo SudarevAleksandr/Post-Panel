e802e1fad5ad04f0121014583d33b428f634474e
Sudarev Aleksandr <sudarev_aleksandr@mail.ru>
2023-12-11 23:45:08 +0300

    create_article_pages

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.tsx
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from 'react-i18next';
-
-const ArticleDetailsPage = () => {
-  const { t } = useTranslation('about');
-
-  return <div>{t('Статья')}</div>;
-};
-
-export default ArticleDetailsPage;
